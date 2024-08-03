0d4e39e06816f621a89a6d137aec6543b68caab2
rohitlandage86 <rohitlandage86@gmail.com>
2024-08-03 11:51:21 +0530

    get data inserted headshot table

--- src/controllers/super-admin/headshot.controller.js
+++ src/controllers/super-admin/headshot.controller.js
@@ -1,14 +1,16 @@
 const pool = require("../../../db");
-const axios = require('axios');
-//error 422 handler
-error422 = (message, res) => {
+const axios = require("axios");
+
+// Error 422 handler
+const error422 = (message, res) => {
   return res.status(422).json({
     status: 422,
     message: message,
   });
 };
-//error 500 handler
-error500 = (error, res) => {
+
+// Error 500 handler
+const error500 = (error, res) => {
   console.log(error);
   return res.status(500).json({
     status: 500,
@@ -16,65 +18,73 @@ error500 = (error, res) => {
     error: error,
   });
 };
-//create headshot...
-const getHeadShotData = async (req, res) => {
 
+// Create headshot...
+const getHeadShotData = async (req, res) => {
   let connection;
   try {
     connection = await pool.connect();
 
-  //sport id is exist...
-  let isSportIdExistQuery = "SELECT * FROM sports WHERE sport_id = $1";
-  const isSportIdExistResult = await connection.query(isSportIdExistQuery, [2])
-  const sportData = isSportIdExistResult.rows[0]
-  if (isSportIdExistResult.rowCount === 0) {
-      return error422('Sport is Not Found', res)
-  }
+    // Check if sport ID exists...
+    const isSportIdExistQuery = "SELECT * FROM sports WHERE sport_id = $1";
+    const isSportIdExistResult = await connection.query(isSportIdExistQuery, [2]);
+    const sportData = isSportIdExistResult.rows[0];
 
-  
-  let Headshot = [];
-  let response = await axios.get(`https://api.sportsdata.io/v3/${sportData.small_name}/headshots/json/Headshots?key=${sportData.api_keys}`);
-  // if (response.data.length > 0) {
-  //     for (let index = 0; index < response.data.length; index++) {
-  //         const element = response.data[index];
-  //         // for (let bettingIndex = 0; bettingIndex < element.Headshot.length; bettingIndex++) {
-  //         //     const Headshot = element.Headshot[bettingIndex]
-  //         //     // if (BettingMarket.BettingBetTypeID == bettingBetTypeData.record_id &&BettingMarket.AnyBetsAvailable == AnyBetsAvailable&&BettingMarket.BettingPeriodTypeID == bettingPeriodTypeData.record_id &&BettingMarket.BettingMarketTypeID == bettingMarketTypeData.record_id) {
-  //         //     //     bettingMarkets.push(BettingMarket)
-  //         //     // }
+    if (isSportIdExistResult.rowCount === 0) {
+      return error422("Sport is Not Found", res);
+    }
 
-  //         // }
-  //     }
-  // } 
-    // Insert headshot 
-    // const headshotQuery = `
-    //   INSERT INTO headshot (
-    //     sport_id, Name, TeamID, Team, Position,
-    //     PreferredHostedHeadshotUrl, HostedHeadshotWithBackgroundUrl, HostedHeadshotNoBackgroundUrl
-    //   ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    // `;
-    // await connection.query(headshotQuery, [
-    //   sport_id,
-    //   Name,
-    //   TeamID,
-    //   Team,
-    //   Position,
-    //   PreferredHostedHeadshotUrl,
-    //   HostedHeadshotWithBackgroundUrl,
-    //   HostedHeadshotNoBackgroundUrl,
-    // ]);
-  // console.log(safeStringify(response, 2));
-  const obj = {};
-obj.circular = obj;
-// const jsonString = JSON.stringify(obj);
+    const response = await axios.get(
+      `https://api.sportsdata.io/v3/${sportData.small_name}/headshots/json/Headshots?key=${sportData.api_keys}`
+    );
+
+    // Insert headshots into the database
+    const insertHeadshotQuery = `
+      INSERT INTO public.headshot (
+        "PlayerID", sport_id, "Name", "TeamID", "Team", "Position",
+        "PreferredHostedHeadshotUrl", "PreferredHostedHeadshotUpdated",
+        "HostedHeadshotWithBackgroundUrl", "HostedHeadshotWithBackgroundUpdated",
+        "HostedHeadshotNoBackgroundUrl", "HostedHeadshotNoBackgroundUpdated"
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+    `;
+
+    for (const headshot of response.data) {
+      try {
+        // Extract only the date part from the timestamp
+        const preferredUpdatedDate = headshot.PreferredHostedHeadshotUpdated
+          ? headshot.PreferredHostedHeadshotUpdated.split('T')[0] + 'T00:00:00Z'
+          : null;
+        const backgroundUpdatedDate = headshot.HostedHeadshotWithBackgroundUpdated
+          ? headshot.HostedHeadshotWithBackgroundUpdated.split('T')[0] + 'T00:00:00Z'
+          : null;
+        const noBackgroundUpdatedDate = headshot.HostedHeadshotNoBackgroundUpdated
+          ? headshot.HostedHeadshotNoBackgroundUpdated.split('T')[0] + 'T00:00:00Z'
+          : null;
+
+        await connection.query(insertHeadshotQuery, [
+          headshot.PlayerID,
+          sportData.sport_id,
+          headshot.Name,
+          headshot.TeamID,
+          headshot.Team,
+          headshot.Position,
+          headshot.PreferredHostedHeadshotUrl,
+          preferredUpdatedDate, // Append time if needed
+          headshot.HostedHeadshotWithBackgroundUrl,
+          backgroundUpdatedDate, // Append time if needed
+          headshot.HostedHeadshotNoBackgroundUrl,
+          noBackgroundUpdatedDate, // Append time if needed
+        ]);
+      } catch (error) {
+        console.error("Error inserting headshot:", headshot.PlayerID, error);
+      }
+    }
+
     return res.status(200).json({
       status: 200,
-      message: "Headshot created successfully.",
-      sport: JSON.stringify(obj)
-      // data: response, 
-
+      message: "Headshots created and inserted successfully.",
+      data: response.data, // Include the response data in the JSON response
     });
-
   } catch (error) {
     error500(error, res);
   } finally {
@@ -82,8 +92,6 @@ obj.circular = obj;
   }
 };
 
-
 module.exports = {
   getHeadShotData,
-
 };
